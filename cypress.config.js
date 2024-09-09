d0e2e412b1892d064f162a5f359931b5044070c2
NotWhoYoureThinkingOf <acurtis1286@gmail.com>
2024-09-09 09:02:43 -0700

    changed url in cypress and js

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://notwhoyourethinkingof.github.io/qa-site'
+    baseUrl: 'https://qa-site-nine.vercel.app/'
   },
 });
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,7 @@
 let cart = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('http://localhost:3000/api/products')
+    fetch('https://qa-site-nine.vercel.app/api/products')
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
